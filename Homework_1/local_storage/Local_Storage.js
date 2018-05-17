9d699605af59b43aaa9b51f565bc2775fe3dfa64
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-05-17 19:58:52 +0200

    ater clear_list

--- Homework_1/local_storage/Local_Storage.js
+++ Homework_1/local_storage/Local_Storage.js
@@ -3,11 +3,13 @@
     var first = document.querySelector("#first");
     var second = document.querySelector("#second");
     var third = document.querySelector("#third");
-    var output1 = document.querySelector("#output1");
     var list = document.querySelector("#list");
+    var add = document.querySelector("input[type='submit']");
+    var input = document.querySelector("input[type='text']");
     
     console.log(first);
-    console.log(output1);
+    console.log(add);
+    console.log(input);
 
     var f1 = function() {
         console.log("Nacisłeś przycisk first");
@@ -29,12 +31,49 @@
     function f3() {
         console.log("nascisłeś przysick Third");
         console.log(localStorage);
+        // console.log(localStorage.length);
+        for(var czosz in localStorage){
+            if(czosz == "length") {
+                break;
+            }
+            console.log("klucz: " + czosz + ", wartoszcz: " + localStorage[czosz]);
+        }
     }
 
+    function add_item(){
+        console.log("jestem w add item");
+        console.log("to jeee z inputa - " + input.value); 
+        localStorage.setItem(localStorage.length+1, input.value);
+        input.value = "";
+    }
 
-    first.addEventListener("click", f1);
+    function fill_list() {
+        let div_task = document.createElement("div");
+        let div_remove = document.createElement("div");
+        let div_item = document.createElement("div");
+        div_task.classList.add("task");
+        div_task.innerHTML = "noowo stworony elementr";
+        div_remove.classList.add("remove");
+        div_remove.innerHTML = "remove";
+
+
+        div_item.appendChild(div_task);
+        div_item.appendChild(div_remove);
+        div_item.classList.add("item");
+
+        list.appendChild(div_item);
+    }
+
+    function clear_list() {
+        console.log(list.firstElementChild);
+        while(list.firstElementChild){
+            list.removeChild(list.firstElementChild);
+        }
+    }
+
+    first.addEventListener("click", fill_list);
     
-    second.addEventListener("click", f2);
+    second.addEventListener("click", clear_list);
 
     third.addEventListener("click", f3);
     
@@ -48,4 +87,9 @@
         }
     });
     
+    add.addEventListener("click", function(e){
+        e.preventDefault();
+    })
+    add.addEventListener("click", add_item);
+
 })();
