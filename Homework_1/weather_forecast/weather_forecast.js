8dd626dac97c107ad415a9a666cac6ce08d3f8b2
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-05-19 23:18:35 +0200

    fucking promises

--- Homework_1/weather_forecast/weather_forecast.js
+++ Homework_1/weather_forecast/weather_forecast.js
@@ -1,75 +1,58 @@
 (function(){
 
-    var button_add = document.querySelector("input[name='button_add']");
     var input = document.querySelector("input[name='input_city']");
     var list = document.querySelector("#list");
     var button_check_weather = document.querySelector("#button_check_weather");
     var button_show_map = document.querySelector("#button_show_map");
+    var button_add_city = document.querySelector("#button_add_city");
     var map_city = new Map();
+    var h3 = document.querySelector("h3");
 
 
 ////////////////////////////////////  City list - add & remove
+    
+ //--------------------------------------------------------------------------------   
 
-    // function fill_map() {
-    //     for(var czosz in localStorage){
-    //         if(czosz == "length") {
-    //             break;
-    //         }
-    //       r map_city.set
-    //     }    
-    // }
-
-    function load_from_local_storage(map) {
-        map = new Map();
+    function load_from_local_storage() {
+        let map = new Map();
         if(localStorage.length) {
             for(let key in localStorage ) {
                     if(key == "length") {
                         break;
                     }        
-                map.set(key);
+
+                map.set( capitalizeFirstLetter(key));
             }
         } else {
             console.log("local storage puste ni ma nic");
         }
 
-        console.log(localStorage);
-        console.log(map);
-
         return map;
     }
 
-    function show_list_from_map(map) {
-        for(let key of map) {
-            console.log("klucz z ahowformmap" + map.get(key) + " i wartosc");
-            console.log(key);
-            // console.log(map.keys() + " " + map.values());
-            fill_list(key);
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    function show_list_from_map() {
+        clear_list();
+        for(let [key, value] of map_city.entries()) {
+            fill_list(key, value);
         }
     }
 
-
-    function show_list() {
-        clear_list();
-            for(var czosz in localStorage){
-                if(czosz == "length") {
-                    break;
-                }
-                fill_list(czosz);
-            }
-    }
-
-    function add_item(){
-        localStorage.setItem(input.value, "whether");
-        input.value = "";
-        show_list();
-    }
-
-    function fill_list(text) {
+    function fill_list(city, temp) {
         let div_task = document.createElement("div");
         let div_remove = document.createElement("div");
         let div_item = document.createElement("div");
         div_task.classList.add("task");
-        div_task.innerHTML = text;
+
+        if(temp) {
+            div_task.innerHTML = city + "       " + temp + " &#8451;";
+        } else {
+            div_task.innerHTML = city;
+        }
+
         div_remove.classList.add("remove");
         div_remove.innerHTML = "remove";
 
@@ -80,83 +63,127 @@
         list.appendChild(div_item);
     }
 
+    function add_city() {
+        localStorage.setItem(input.value, "whether");
+        input.value = "";        
+        map_city = load_from_local_storage();
+        show_list_from_map();
+    }
+
     function clear_list() {
         while(list.firstElementChild){
             list.removeChild(list.firstElementChild);
         }
     }
 
-
-    function remove_item_from_storage(remove_target){
-
+    function remove_city(target) {
         if(confirm("Napewno chcesz usunąć to miasto?")){
-            localStorage.removeItem(remove_target.previousSibling.innerHTML);
-            remove_target.parentNode.remove();
+            console.log(target.previousSibling.innerHTML)
+            localStorage.removeItem(target.previousSibling.innerHTML);
+            map_city =  load_from_local_storage();
+            show_list_from_map();
+            // remove_target.parentNode.remove();
         };
     }
 
-    list.addEventListener("click", function(e){
-        if(e.target.innerHTML == "remove"){
-            remove_item_from_storage(e.target);
-        }
-    });
 
 /////////////////////////////////////////////////////////////////////
 
-    // document.addEventListener("DOMContentLoaded", show_list);
-    // document.addEventListener("DOMContentLoaded", load_from_local_storage);
 
     document.addEventListener("DOMContentLoaded", function(){
 
-        console.log(map_city);
-
         map_city = load_from_local_storage();
-        
-        console.log(map_city);
-        show_list_from_map(map_city);
+        check_weather();
 
+        // h3.innerHTML = "dupa";
+        // get_temp();
+        
+        // console.log(map_city);
+        // show_list_from_map();
 
 
     });
 
 
     function send_data(city) {
+        let temp;
+        let city_name;
         let url = "http://api.openweathermap.org/data/2.5/forecast?q="+ city + "&appid=09d095681879bfdc3462857a2653dc8c&units=metric";
         let data = null;
         let xmlhttp = new XMLHttpRequest();
 
-        console.log("JESTEM W KURWA FUNKCJI");
+        xmlhttp.open("GET", url, false);
 
-        xmlhttp.open("GET", url, true);
+        // xmlhttp.onreadystatechange = function() {
 
-        xmlhttp.onreadystatechange = function() {
+        //     if  (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        //         data = JSON.parse(xmlhttp.responseText);
+        //     } 
 
-            if  (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                data = JSON.parse(xmlhttp.responseText);
-            } 
+        // };
+        // let p = new Promise(function(resolve, reject){   
 
-        };
+            xmlhttp.onload = function() {
+                if (xmlhttp.status == 200) {
 
-        xmlhttp.onloadend = function() {
-            console.log("Wczytalo sie:");
-            console.log(data);
-            console.log(data.city.name);
-         map_city.set(data.city.name,average_temp(data));
-        }
+                    data = JSON.parse(xmlhttp.responseText);
+                    temp = Math.trunc(average_temp(data));
+                    map_city.set(city,temp);
+
+                    // console.log("temp z onlad " + temp);
+                    // resolve(temp);
+
+                } else {
+                    // reject("error");
+                }
+            }
+
+        // });
+
+        // xmlhttp.onloadend = function() {
+
+        //     city_name = data.city.name;
+        //     temp = Math.trunc(average_temp(data));
+        //     map_city.set(city,temp);
+        //     return temp;
+        // }
 
         xmlhttp.send();
+
+        // return p;
+
     }
 
     function check_weather() {
 
-        for(let city in localStorage) {
-            if(city == "length") {
-                break;
-            }
-            send_data(city);
-        }
-        show_map();
+        //  let p = new Promise(function(resolve, reject){ 
+
+            for (let key of map_city.keys()) {
+
+
+                 send_data(key);
+            };
+           
+
+        //  });
+
+        // p.then(show_list_from_map());
+  
+        // return p;
+
+         show_map();
+         show_list_from_map();
+
     }
+
+    // function get_temp() {
+
+    //     check_weather();
+
+
+
+
+    // }
 
     function average_temp(data) {
 
@@ -172,15 +199,17 @@
 
     function show_map() {
         console.log("JESTEM W SZOW MAP");
-           map_city.values();
-           map_city.keys();
-        for(let each in map_city.entries()){
-            console.log(each);
-        }
+           console.log(map_city.values());
+           console.log(map_city.keys());
     }
 
-
-    button_add.addEventListener("click", add_item);
+    button_add_city.addEventListener("click", add_city);
     button_check_weather.addEventListener("click", check_weather);
     button_show_map.addEventListener("click", show_map);
+
+    list.addEventListener("click", function(e){
+        if(e.target.innerHTML == "remove"){
+            remove_city(e.target);
+        }
+    });
 })();
