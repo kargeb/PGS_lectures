f84ef5cc4193e188ad3a3c9e3f6cb10598d2acfd
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-05-23 14:23:16 +0200

    DONE

---
+++ Homework/modules/index.js
@@ -0,0 +1,20 @@
+console.log("glowna");
+
+var one = document.querySelector(".one");
+var two = document.querySelector(".two");
+var three = document.querySelector(".three");
+
+one.addEventListener("click", function(){
+    console.log("one we w");
+    window.location="console_program/console.html";
+});
+
+two.addEventListener("click", function(){
+    console.log("one we w");
+    window.location="todo/todo.html";
+});
+
+three.addEventListener("click", function(){
+    console.log("one we w");
+    window.location="weather_forecast/weather_forecast.html";
+});
