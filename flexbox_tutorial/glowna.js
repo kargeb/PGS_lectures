ea870304d94dc08a6833a242aeba0a4eb19704e9
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-05-23 14:11:18 +0200

    go gog o

---
+++ flexbox_tutorial/glowna.js
@@ -0,0 +1,10 @@
+console.log("glowna");
+
+var one = document.querySelector(".one");
+var two = document.querySelector(".two");
+var three = document.querySelector(".three");
+
+one.addEventListener("click", function(){
+    console.log("one we w");
+    window.location="http://onet.pl";
+});
